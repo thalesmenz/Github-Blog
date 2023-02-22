e9d6c027026f3524464200915e9f447c97f5d904
thales menzner <thalesmenzz@gmail.com>
2023-02-22 14:54:45 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
+
+function App() {
+  
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>hello word</h1>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
+
+export default App
---
+++ src/styles/global.ts
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        background: ${props => props.theme["base-background"]};
+        color: ${props => props.theme["base-text"]};
+    }
+
+    body, input-security, textarea, button, p {
+        font: 400 1rem Nunito, sans-serif;
+    }
+`
